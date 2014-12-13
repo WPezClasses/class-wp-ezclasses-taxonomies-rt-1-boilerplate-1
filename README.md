bd753662dde0f25b5925446a42e5402b973f4bb4
alchemyunited <mark.simchock@alchemyunited.com>
2014-12-12 21:46:36 -0500

    push up 1

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-class-wp-ezclasses-taxonomies-rt-1-boilerplate-1
-================================================
+# Org: WPezClasses
+### Product: Class_WP_ezClasses_Taxonomies_RT_1_Boilerplate_1
 
-WordPress taxonomies The ezWay. This is the boilerplate to extend class-wp-ezclasses-taxonomies-rt-1
+##### WordPress taxonomies The ezWay. This is the boilerplate to extend class-wp-ezclasses-taxonomies-rt-1.
+
+Copy this into your plugin or theme; do the TODOs; and voila you are done. 
+
+=======================================================================================
+
+#### WPezClasses: Getting Started
+- https://github.com/WPezClasses/wp-ezclasses-docs-getting-started
+
+=======================================================================================
+
+#### Overview
+
+TODO (if it's even necessary)
---
+++ class-wp-ezclasses-taxonomies-rt-1-boilerplate-1.php
@@ -0,0 +1,74 @@
+<?php
+/** 
+ * Wordpress register_taxonomy() The ezWay.
+ *
+ * Truth be told, the primary benefit here is automating the labels. That said, having the minimal TODOs laid out for you helps too.   
+ *
+ * PHP version 5.3
+ *
+ * LICENSE: TODO
+ *
+ * @package WPezClasses
+ * @author Mark Simchock <mark.simchock@alchemyunited.com>
+ * @since 0.5.1
+ * @license TODO
+ */
+ 
+/**
+ * == Change Log == 
+ *
+ * -- 0.5.0 - Mon 8 Dec 2014
+ * ---- Pop the champagne!
+ */
+ 
+/**
+ * == TODO == 
+ *
+ *
+ */
+
+// No WP? Die! Now!!
+if (!defined('ABSPATH')) {
+	header( 'HTTP/1.0 403 Forbidden' );
+    die();
+}
+
+if ( ! class_exists('Class_WP_ezClasses_Taxonomies_RT_1_Boilerplate_1_TODO') ) {
+  class Class_WP_ezClasses_Taxonomies_RT_1_Boilerplate_1_TODO extends Class_WP_ezClasses_Taxonomies_RT_1{
+  
+    
+	/**
+	 * This is where your magic happens. The idea here is to (re) define as little as possible. Of course you
+	 * can add args to these various arrays[key] and they'll overide the defaults. The point is, this is all the 
+	 * TODOs you need to get a taxonomy up and running. 
+	 */
+	protected function taxonomy_todo(){
+	
+	  $str_taxonomy = 'TODO';
+	
+	  $arr_taxonomy_todo = array(
+	  
+	    'taxonomy'		=> $str_taxonomy,
+		'object_type'	=> array(),							// TODO - aka array of post_type(s) to attach this to.
+		'arguments'		=> array(
+		  'hierarchical'	=> false,						// TODO. fyi: default is false
+		  'query_var'		=> $str_taxonomy,				// default is the 'taxonomy', or if ya want, do your own here
+		  'labels'			=> array(
+		    'name'				=> 'TODO: name',			// name
+		    'singular_name'		=> 'TODO: singular_name',	// labels: 'singular_name'
+			'menu_name' 		=> 'TODO: name',			// labe
+			),
+		  'rewrite' => array(
+		    'slug' => 'TODO-slug',    // Note: if the slug is the same as $str_taxonomy then this can be removed / commented out. 
+			),			
+		  ),
+	    );
+	  
+	  return $arr_taxonomy_todo;
+	}
+
+  }
+} 
+
+// TODO = update class name and then uncomment
+// $obj_instantiate = Class_WP_ezClasses_Taxonomies_RT_1_Boilerplate_1_TODO::ez_new();
